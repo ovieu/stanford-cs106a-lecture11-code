9068ba26ba7724870c08097cb0c61ad44adece4d
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2018-01-15 12:38:01 -0500

    Exception example code

---
+++ src/com/neo/cs106aclass/ExceptionTest.java
@@ -0,0 +1,30 @@
+package com.neo.cs106aclass;
+
+import java.io.IOException;
+
+public class ExceptionTest {
+
+    public static void main(String[] args) {
+
+//        try {
+//            throw new NullPointerException();
+//        } catch(NullPointerException e) {
+//            throw new NullPointerException(e);
+//        }
+
+        System.out.println("The code continues");
+
+        /*System.out.println(" ");
+        System.out.println("Ioexception starts here: ");
+
+        try {
+            throw new IOException();
+        } catch(IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+            //e.getMessage();
+        }*/
+        System.out.println("2 * 2: " + 2 * 2);
+
+    }
+}
