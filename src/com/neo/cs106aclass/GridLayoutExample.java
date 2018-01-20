2321c98736c12ce3f429e3fc50c73fa30f703acc
Ovie Umukoro <ovie.u.umukoro@gmail.com>
2018-01-19 23:54:03 -0500

    Grid layout source code

--- src/com/neo/cs106aclass/GridLayoutExample.java
+++ src/com/neo/cs106aclass/GridLayoutExample.java
@@ -1,4 +1,28 @@
 package com.neo.cs106aclass;
 
-public class GridLayoutExample {
+import acm.program.Program;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class GridLayoutExample extends Program {
+    public static void main(String[] args) {
+        new GridLayoutExample().start(args);
+    }
+
+    public void init() {
+        setLayout(new GridLayout(2, 3));
+        add(new JButton("button 1"));
+        add(new JButton("button 2"));
+        add(new JButton("button 3"));
+        add(new JButton("button 4"));
+        add(new JButton("button 5"));
+        add(new JButton("button 6"));
+        addActionListeners();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
